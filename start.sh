002035056930cc1c7411f63ba8702ff7ca047d25
harrisonprest70 <72754761+harrisonprest70@users.noreply.github.com>
2021-03-17 15:38:02 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,11 +11,8 @@ ls -al "$DIR/docs"
 
 cp -R "$DIR/docs" "$GITHUB_WORKSPACE/docs"
 
-sudo apt-get install doxygen 
+sudo apt-get install doxygen python3-setuptools python3-sphinx 
 echo "ciao"
-
-echo "ao"
-sudo apt-get python3-sphinx 
 pip3 install "breathe==4.12.0" exhale sphinx_rtd_theme
 
 cd docs
