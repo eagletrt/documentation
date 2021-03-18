243611e531216d376bb2fc15df21b24dc4f7c4a3
harrisonprest70 <72754761+harrisonprest70@users.noreply.github.com>
2021-03-18 15:54:07 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 set -e
 
 INPUT_AUTHOR=${INPUT_AUTHOR:-'EagleTRT Team'}
+PROJECT_NAME=${PROJECT_NAME:-'EagleTRT Project'}
 
 DIR="$(dirname "${BASH_SOURCE[0]}")"
 
@@ -18,6 +19,7 @@ pip3 install "breathe==4.12.0" exhale sphinx_rtd_theme
 cd docs
 
 sed -i "s/{{AUTHOR}}/$INPUT_AUTHOR/" conf.py
+sed -i "s/{{PROJECT}}/$PROJECT_NAME/" conf.py
 
 sphinx-build --version
 sphinx-build -b html ../docs ../docs/_build
