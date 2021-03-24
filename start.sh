ffb8aa0bf94a882953bfbf14820ea9f293077990
harrisonprest70 <72754761+harrisonprest70@users.noreply.github.com>
2021-03-24 10:12:19 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -88,8 +88,7 @@ do
             filepng=${file/"_"/"__"}
             png="html/$filepng"_"8$ext"__"dep__incl.png"
             cd docs/_build/api
-            echo "$html"
-            #sed -i "s+<div class=\"wy-nav-content\">+<div class=\"libGraph\"><img src=\"$png\" alt=\"prova\"></div><div class=\"wy-nav-content\">+" $html
+            sed -i "s+<div class=\"wy-nav-content\">+<div class=\"libGraph\"><img src=\"$png\" alt=\"prova\"></div><div class=\"wy-nav-content\">+" $html
             cd - &> /dev/null
         fi    
     done   
