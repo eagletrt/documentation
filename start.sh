20dfff84387cec79341b55a876159aaa9d4d333f
harrisonprest70 <72754761+harrisonprest70@users.noreply.github.com>
2021-04-15 11:38:10 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -64,37 +64,44 @@ do
         end=$(( end - 1 ));
     fi
     dir=""
+    dirR=""
     filenames=$(ls -R $val1)   
     for f in $filenames */
     do
         f=${f%%:*}
         echo $f;
-        if [[ -d $f || -d $val1"/"$f ]]; 
+        if [[ -d $f || -d $dirR"/"$f ]]; 
         then
-            dir=${f/"/"/"_"}
+            
+            dirR=$dirR"/"$f
+            dir=${f//"/"/"_"}
             dir="_$dir"
         else
+            dirR=""
             ext=${f##*.}
-            case $ext in
-                c | cpp | h | hpp| py | js | java)
-                    file=${f%%.*}
-                    html="file$dir"_"$file"."$ext.html"
-                    filepng=${file/"_"/"__"}
-                    if [ $ext = "h" ];
-                    then
-                        png="html/$filepng"_"8$ext"__"dep__incl.png"
-                    else
-                        png="html/$filepng"_"8$ext""__incl.png"
-                    fi
-                    cd docs/_build/api
-                    echo "Add dependencies graph to $html"
-                    sed -i "s+<div role=\"main\" class=\"document\"+<div class=\"libGraph\"><img src=\"$png\" alt=\"depGraph\" style=\"position:relative;left: 5%;\"></div><div role=\"main\" class=\"document\"+" $html
-                    cd - &> /dev/null
-                    ;;
-                *)
-                    echo "$ext files not documented"
-                    ;;
-            esac 
+            if [ $ext != $f ];
+            then
+                case $ext in
+                    c | cpp | h | hpp| py | js | java)
+                        file=${f%%.*}
+                        html="file$dir"_"$file"."$ext.html"
+                        filepng=${file/"_"/"__"}
+                        if [ $ext = "h" ];
+                        then
+                            png="html/$filepng"_"8$ext"__"dep__incl.png"
+                        else
+                            png="html/$filepng"_"8$ext""__incl.png"
+                        fi
+                        cd docs/_build/api
+                        echo "Add dependencies graph to $html"
+                        sed -i "s+<div role=\"main\" class=\"document\"+<div class=\"libGraph\"><img src=\"$png\" alt=\"depGraph\" style=\"position:relative;left: 5%;\"></div><div role=\"main\" class=\"document\"+" $html
+                        cd - &> /dev/null
+                        ;;
+                    *)
+                        echo "$ext files not documented"
+                        ;;
+                esac 
+            fi
         fi
     done
 done
